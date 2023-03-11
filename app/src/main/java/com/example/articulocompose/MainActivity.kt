4c2e327f90ed785b1519c222f1132b734de33e1b
mmrach <mmrach@gmail.com>
2023-03-11 14:03:31 +0100

    InitialComit

---
+++ app/src/main/java/com/example/articulocompose/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.example.articulocompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.articulocompose.ui.theme.ArticuloComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ArticuloComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MainContent()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainContent() {
+    Article(
+        title = stringResource(R.string.article_title),
+        shortDescription = stringResource(R.string.article_short_desc),
+        longDescription = stringResource(R.string.article_description),
+        imagePainter = painterResource(R.drawable.bg_compose_background)
+    )
+}
+
+@Composable
+private fun Article(
+    title:String,
+    shortDescription:String,
+    longDescription:String,
+    imagePainter:Painter,
+    modifier: Modifier = Modifier,
+){
+    Column(modifier = modifier) {
+        Image(painter = imagePainter, contentDescription = null)
+        Text(
+            text=title,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text=shortDescription,
+            textAlign= TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text=longDescription,
+            textAlign= TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ArticuloComposeTheme {
+        MainContent()
+    }
+}
